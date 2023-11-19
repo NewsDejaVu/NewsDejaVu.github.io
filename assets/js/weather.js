45259d4da07f7de49e18fafd05b1f6d4722a405a
NewsDejaVu <newsdejavu.dellresearch@gmail.com>
2023-11-19 16:45:56 -0500

    Found a better solution to the weather box

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -15,16 +15,31 @@ async function getWeather() {
   // Making an API call (request) 
   // and getting the response back 
   const response1 = await fetch((api_url + Latitude + "," + Longitude)); 
+
+  if (!response1.ok){
+
+    document.getElementById("weather-data").innerHTML = ""
+
+  } else if (response1.ok){
+
+    const data1 = await response1.json(); 
+
+    const response2 = await fetch(data1.properties.forecast); 
+
+    if (!response2.ok) {
+
+      document.getElementById("weather-data").innerHTML = ""
+
+    } else if (response2.ok) {
+
+      const data2 = await response2.json()
+
+      document.getElementById("weather-data").innerHTML = data2.properties.periods[0].detailedForecast
+
+    }
+
+  }
   
-  // Parsing it to JSON format 
-  const data1 = await response1.json(); 
-
-  const response2 = await fetch(data1.properties.forecast); 
-
-  const data2 = await response2.json()
-
-  document.getElementById("weather-data").innerHTML = data2.properties.periods[0].detailedForecast
-    
 } 
 
 // Calling the function every hour
